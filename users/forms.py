bd2557300aef168e5469b35ee6213d0530908690
Devisingh Dangi <devisinghdangi786@gmail.com>
2026-09-04 13:22:27 +0000

    profile and user custom form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'photo']
+
+
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
