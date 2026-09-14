532cf63707c88143420b8e87d6033a34bc1b8f4d
Devisingh Dangi <devisinghdangi786@gmail.com>
2026-09-14 11:50:10 +0000

    like_post funtionality url pattern

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create_post
+from . import views
 
 urlpatterns = [
-    path('create/', create_post, name='create_post'),
+    path('create/', views.create_post, name='create_post'),
+    path('feed/', views.feed, name='feed'),
+    path('like/', views.like_post, name='like_post'),
 ]
