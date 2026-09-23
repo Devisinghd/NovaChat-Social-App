395bfae3c5b876d7325748fbaa622120aaa1d4cd
Devisingh Dangi <devisinghdangi786@gmail.com>
2026-09-23 11:05:51 +0000

    created Comment model

--- posts/models.py
+++ posts/models.py
@@ -20,3 +20,14 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __Str__(self):
+        return self.body
