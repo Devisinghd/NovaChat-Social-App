6ccbf9099787c26c405cca6c6cdc26fef40c0635
Devisingh Dangi <devisinghdangi786@gmail.com>
2026-09-23 12:19:10 +0000

    added excluded fields in forms

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Post
+from .models import Post, Comments
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['image', 'caption', 'title']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('body',)
+        exclude = ('posted_by', 'post')
