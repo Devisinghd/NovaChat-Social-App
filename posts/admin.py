c4c2311119922d2eec360bf5fe99e39dade7db2a
Devisingh Dangi <devisinghdangi786@gmail.com>
2026-09-23 12:17:56 +0000

    changed the model name from comment to comments

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post,Comment
+from .models import Post, Comments
 
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Comments)
